4226960884164ff8e09c80d74ca48aecc53edcdc
YvetteT1 <yvette.kmh@gmail.com>
2023-10-24 11:46:50 +0900

    change

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      배포를 다시 했습니다. by ablearn
+      테스트 테스트
     </main>
   )
 }
